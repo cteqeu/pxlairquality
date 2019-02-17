7e7f98f54c2b786ccab37d5cced6dce3393a0fe9
Bryan Honof <bryanhonof@gmail.com>
2019-02-17 11:51:43 +0100

    Added possibility to GET asset states from devices trough curl.

--- Software/api_scripts/test.sh
+++ Software/api_scripts/test.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-post_url=""
+method="$7";
+device_id="$1";
+asset="$2";
+authorization="$6";
+from="$3"
+to="$4"
+page="$5"
+url="https://api.allthingstalk.io";
 
-if [ "$1" != "" ]; then
 
+curlGet ()
+{
+    curl -X GET "${url}/device/${device_id}/asset/${asset}/states?from=${from}&to=${to}&page=${page}" \
+         -H "Authorization: Bearer ${authorization}"
+}
+
+if [ "$#" -ge 1 ]; then
+    curlGet
 else
     echo "No arguments supplied"
     exit
