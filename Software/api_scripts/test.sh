43b4711a6e764413742b06953a62c73956c996e8
Bryan Honof <bryanhonof@gmail.com>
2019-02-17 12:46:04 +0100

    Added functionality to get latest asset state and aggregated data.

--- Software/api_scripts/test.sh
+++ Software/api_scripts/test.sh
@@ -12,8 +12,21 @@ page="$5"
 
 curlGet()
 {
-    curl -X ${method} "${url}/device/${device_id}/asset/${asset}/states?from=${from}&to=${to}&page=${page}" \
-         -H "Authorization: Bearer ${authorization}"
+    case "$from" in
+        "latest")
+            curl -X ${method} "${url}/device/${device_id}/asset/${asset}/state" \
+                 -H "Authorization: Bearer ${authorization}"
+            ;;
+        "arregated")
+            curl -X ${method} "${url}/device/${device_id}/asset/${asset}/states?resolution=day&from=${from}&to=${to}" \
+                 -H "Authorization: Bearer ${authorization}"
+            ;;
+        *)
+            curl -X ${method} "${url}/device/${device_id}/asset/${asset}/states?from=${from}&to=${to}&page=${page}" \
+                 -H "Authorization: Bearer ${authorization}"
+            ;;
+    esac
+
 }
 
 usage()
